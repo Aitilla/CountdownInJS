4518117da24a57d41781ad240a226c8f459284c4
Aitilla <jhertaas@gmail.com>
2024-01-11 11:05:22 +0100

    fullyworkingCounter

--- 3rdCounter.js
+++ 3rdCounter.js
@@ -1,18 +1,24 @@
 
 function countDown(){
 
-    var timeOut = 0
+    let timeOut = 0
 
     if (timeLeft > timeOut){
         console.log('hello world')
         timeLeft -= 1;
         console.log(timeLeft)
+        displayTime.innerText = timeLeft
     } else{
         clearInterval(interval)
+        displayTime.innerText = 'You are out of time, please try again'
     }
 }
 
-var timeLeft = 5
+var timeLeft = 60
+
+var displayTime = document.createElement('p')
+
+var div = document.getElementById('display')
+div.appendChild(displayTime)
 
 interval = setInterval(countDown, 1000)
-
