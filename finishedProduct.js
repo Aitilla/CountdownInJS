40967394fdd55430438d8d4d6269510ec0e3087a
Aitilla <jhertaas@gmail.com>
2024-01-11 11:35:35 +0100

    Finished

--- finishedProduct.js
+++ finishedProduct.js
@@ -6,16 +6,18 @@ function countDown(){
     if (timeLeft > timeOut){
         timeLeft -= 1;
         console.log(timeLeft)
-        displayTime.innerText = timeLeft
+        displayTime.innerText = `You have ${timeLeft} seconds left to authenticate`
     } else{
         clearInterval(interval)
-        displayTime.innerText = 'You are out of time, please try again'
+        displayTime.innerText = 'You are out of time to authenticate, please try again.'
     }
 }
 
 var timeLeft = 60
 
 var displayTime = document.createElement('p')
+displayTime.classList.add('desiredClassName')
+displayTime.id = 'desiredID'
 
 var div = document.getElementById('display')
 div.appendChild(displayTime)
